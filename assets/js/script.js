8eeeb392f33a9dbcf08f56b2177f68cf31bf1513
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-31 09:23:10 +0100

    exo corrige

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
+//CETET FONCTION PERMET DE MONTRER LE TEXTE AU CLICK (ONCLICK)
 function show() {
   document.getElementById('text').style.display = 'block';
 }
-
+//CETTE FONCTION PERMET DE MASQUE LE TEXTE AU CLICK
 function hide() {
   document.getElementById('text').style.display = 'none';
 }
+// POUR CACHER LE TEXTE AU DEMARRAGE
+document.getElementById('text').style.display = 'none';
